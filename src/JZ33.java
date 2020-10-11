ba8877e29d663d8a28ffbcda8a8d7776c39ee6eb
yuqi0706 <wangyuqi2000@outlook.com>
2020-10-11 13:37:30 +0800

    33

---
+++ src/JZ33.java
@@ -0,0 +1,13 @@
+public class JZ33 {
+    public int GetUglyNumber_Solution(int index) {
+        return 0;
+    }
+
+    boolean isUgly(int n) {
+        if (n == 1)
+            return true;
+        if(n%2==0&&n%3==0&&n%5==0)
+            return
+    }
+
+}
